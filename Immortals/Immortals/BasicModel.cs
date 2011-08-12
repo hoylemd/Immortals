bc5e0e9dd1ca77313ee4735ca224a2c73ee6b56c
Michael D. Hoyle <hoylemd@gmail.com>
2011-08-12 12:23:50 -0400

    normalized and cleaned up BasicModel class

--- Immortals/Immortals/BasicModel.cs
+++ Immortals/Immortals/BasicModel.cs
@@ -8,40 +8,42 @@ using Microsoft.Xna.Framework.Graphics;
 namespace Immortals
 {
     /// <summary>
-    /// Class to hold information about a basic model
-    /// </summary>
+    /// Class to hold information about a basic model</summary>
     class BasicModel
     {
+        // Model drawing information
         public Model model { get; protected set; }
         protected Matrix world = Matrix.Identity;
 
         /// <summary>
-        /// Constructor
-        /// </summary>
+        /// Constructor</summary>
         /// <param name="m"> The model object to use</param>
         public BasicModel(Model m)
         {
+            // Save data
             this.model = m;
         }
 
         /// <summary>
-        /// Overrideable accessor for world
-        /// </summary>
-        /// <returns> this model's World matrix</returns>
+        /// Overrideable accessor for world</summary>
+        /// <returns> 
+        /// this model's World matrix</returns>
         public virtual Matrix GetWorld()
         {
+            // return the world variable
             return this.world;
         }
 
         /// <summary>
-        /// Empty Update function for overriding
-        /// </summary>
-        public virtual void Update() { }
+        /// Empty Update function for overriding.</summary>
+        public virtual void Update() 
+        { 
+        }
 
         /// <summary>
-        /// Function to draw this model
-        /// </summary>
-        /// <param name="camera"> The camera to draw this model to</param>
+        /// Function to draw this model.</summary>
+        /// <param name="camera"> 
+        /// The camera to draw this model to.</param>
         public void Draw(Camera camera)
         {
             // grab the list of bone transformations
@@ -66,11 +68,13 @@ namespace Immortals
         }
 
         /// <summary>
-        /// Function to determine if two models have collided
-        /// </summary>
-        /// <param name="otherModel"> The other Model to check</param>
-        /// <param name="otherWorld"> The other model's world transformation</param>
-        /// <returns></returns>
+        /// Function to determine if two models have collided.</summary>
+        /// <param name="otherModel"> 
+        /// The other Model to check</param>
+        /// <param name="otherWorld"> 
+        /// The other model's world transformation</param>
+        /// <returns>
+        /// True is these models have collided. False if not.</returns>
         public bool CollidesWith(Model otherModel, Matrix otherWorld)
         {
             // Loop through each modelMesh in both objects and compare
@@ -79,9 +83,11 @@ namespace Immortals
             {
                 foreach (ModelMesh hisModelMeshes in otherModel.Meshes)
                 {
-                    if (myModelMeshes.BoundingSphere.Transform(
-                        GetWorld()).Intersects(
-                        hisModelMeshes.BoundingSphere.Transform(otherWorld)))
+                    if (
+                        myModelMeshes.BoundingSphere.Transform( 
+                            GetWorld()).Intersects(
+                                hisModelMeshes.BoundingSphere.Transform(
+                                    otherWorld)))
                         return true;
                 }
             }
