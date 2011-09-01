7185e367c358e3714b35c29d0c41ff8bbd352dd4
hoylemd <hoylemd@gmail.com>
2011-09-01 18:30:16 -0400

    Fleshed out basic data and constructor of Cylinder class.

--- Immortals/Immortals/Cylinder.cs
+++ Immortals/Immortals/Cylinder.cs
@@ -2,10 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
 
 namespace Immortals
 {
+    /// <summary>
+    /// Class to represent a cylinder shape.
+    /// </summary>
     class Cylinder
     {
+        public float Height { get; protected set; }
+        public float Radius { get; protected set; }
+        public Matrix World { get; protected set; }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="Height">The height of the cylinder</param>
+        /// <param name="Radius">The Radius of the cylinder</param>
+        /// <param name="World">
+        /// The transformation matrix of the cylinder</param>
+        public Cylinder(float Height, float Radius, Matrix World)
+        {
+            // Save data
+            this.Height = Height;
+            this.Radius = Radius;
+            this.World = World;
+        }
+        
     }
 }
