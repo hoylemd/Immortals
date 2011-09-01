244bb84b6b5468923ffebf6f258b10cafa239735
hoylemd <hoylemd@gmail.com>
2011-09-01 18:44:11 -0400

    Fleshed out box class with data and constructor.

--- Immortals/Immortals/Box.cs
+++ Immortals/Immortals/Box.cs
@@ -2,10 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
 
 namespace Immortals
 {
+    // Class to represent a rectangular Prism
     class Box
     {
+        public float Width { get; protected set; }
+        public float Height { get; protected set; }
+        public float Depth { get; protected set; }
+        public Matrix World { get; protected set; }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="Width">The width of the box (X-axis)</param>
+        /// <param name="Height">The height of the box (Y-axis)</param>
+        /// <param name="Depth">The depth of the box (Z-axis)</param>
+        /// <param name="World">
+        /// The matrix representing world transforms</param>
+        public Box(float Width, float Height, float Depth, Matrix World)
+        {
+            // Save data
+            this.Width = Width;
+            this.Height = Height;
+            this.Depth = Depth;
+            this.World = World;
+        }
     }
 }
