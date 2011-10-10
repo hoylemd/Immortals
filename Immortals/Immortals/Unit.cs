f3bf8107ef36ac75ec7f6e7d851c3b9bea52c9f8
hoylemd <hoylemd@gmail.com>
2011-10-09 22:48:37 -0400

    Added some early Unit stuff.

--- Immortals/Immortals/Unit.cs
+++ Immortals/Immortals/Unit.cs
@@ -9,6 +9,30 @@ namespace Immortals
     /// Class to represent a tactical unit</summary>
     class Unit
     {
+        // the list of models for the unit
+        List<BasicModel> modelList;
 
+        // the game stats
+        int Mv;
+
+        // aesthetic data
+        String name;
+
+        // Parent pointer
+        ImmortalsEngine engine;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="engine"> Pointer to the parent object.</param>
+        /// <param name="Mv"> The Mv stat for the object.</param>
+        /// <param name="name"> The name of this unit.</param>
+        public Unit(ImmortalsEngine engine, int Mv, String name)
+        {
+            // Store data
+            this.engine = engine;
+            this.Mv = Mv;
+            this.name = name;
+        }
     }
 }
